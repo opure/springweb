52fbb9d50a55046a105a1226d24cd3da8b220c3a
opur <caohao@aukeys.com>
2017-08-07 15:39:13 +0800

    111

--- src/main/java/com/oneler/springweb/web/IndexController.java
+++ src/main/java/com/oneler/springweb/web/IndexController.java
@@ -1,15 +1,1 @@
-package com.oneler.springweb.web;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/")
-public class IndexController {
-
-    @RequestMapping("/")
-    public String index() {
-        return "hello world ~~";
-    }
-
-}
+package com.oneler.springweb.web;import org.springframework.web.bind.annotation.RequestMapping;import org.springframework.web.bind.annotation.RestController;@RestController@RequestMapping("/")public class IndexController {    @RequestMapping("/")    public String index() {        return "hello world zzzzzz ~~";    }}
